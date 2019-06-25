386912bb15c02cbc3efcc3fd63cc56d32fb67e88
xuxunxiong <1024866290@qq.com>
2019-06-25 21:28:19 +0800

    second commit

--- app/spider/yushu_book.py
+++ app/spider/yushu_book.py
@@ -1,7 +1,9 @@
-from httper import Httper
+from app.libs.httper import Httper
+from flask import current_app
 
 
 class YuShuBook:
+    # per_page = 15
     isbn_url = 'http://t.yushu.im/v2/book/book/isbn/{}'
     key_url = 'http://t.yushu.im/v2/book/search?q={}&count={}&start={}'
 
@@ -12,7 +14,11 @@ class YuShuBook:
         return result
 
     @classmethod
-    def search_by_key(cls, key, count=15, start=0):
-        url = cls.key_url.format(key, count, start)
+    def search_by_key(cls, key, page=1):
+        url = cls.key_url.format(key, current_app.config['PER_PAGE'], cls.calculate_start(page))
         result = Httper.get(url)
         return result
+
+    @classmethod
+    def calculate_start(cls, page):
+        return (page - 1) * current_app.config['PER_PAGE']
--- app/web/book.py
+++ app/web/book.py
@@ -1,25 +1,36 @@
-from helper import is_isbn_or_key
-from yushu_book import YuShuBook
-from flask import jsonify
+from app.forms.book import SearchForm
+from app.libs.helper import is_isbn_or_key
+from app.spider.yushu_book import YuShuBook
+from flask import jsonify, request
 from . import web
 
 
 # 蓝图
 
 
-@web.route('/book/search/<q>/<page>')
-def search(q, page):
+@web.route('/book/search/')
+def search():
     """
 
     :param q: key_word or isbn
     :param page:
     :return:  result
     """
-    isbn_or_key = is_isbn_or_key(q)
-    if isbn_or_key == 'isbn':
-        result = YuShuBook.search_by_isbn(q)
-    if isbn_or_key == 'key':
-        result = YuShuBook.search_by_key(q)
+    #
+    # q = request.args['q']
+    # page = request.args['page']
 
-    # return json.dumps(result), 200, {'content-type': 'application/json'}
-    return jsonify(result)
+    form = SearchForm(request.args)
+    if form.validate():
+        q = form.q.data.strip()
+        page = form.page.data
+        isbn_or_key = is_isbn_or_key(q)
+        if isbn_or_key == 'isbn':
+            result = YuShuBook.search_by_isbn(q)
+        if isbn_or_key == 'key':
+            result = YuShuBook.search_by_key(q, page)
+
+        # return json.dumps(result), 200, {'content-type': 'application/json'}
+        return jsonify(result)
+    else:
+        return jsonify(form.errors)
